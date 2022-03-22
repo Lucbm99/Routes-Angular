9423e916d7223cfb48d00d34e4f39a1b9576a0c2
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-03-21 21:51:29 -0300

    Fix - router link navegação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
-
+  template: `
+    <app-menu></app-menu>
+    <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent {}
