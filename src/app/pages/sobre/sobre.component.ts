c20eea93c17d1b2ba465e324214f72b55896d3a0
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-03-22 22:38:38 -0300

    fix - rotas ativas + parâmetros + redirecionamento

--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sobre',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobreComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(
+      res => console.log(res.id, res.username)
+    )
+
+    this.activatedRoute.queryParams.subscribe(
+      res => console.log(res)
+    )
+
+    setInterval(() => {
+      //navegações internas no sistema
+      // this.router.navigate(['404']);
+
+      //navegações para páginas (reload)
+      this.router.navigateByUrl('404');
+    }, 5000)
   }
 
 }
